7e0fc8d2997a0746883674dcd75e089022e77807
jmurph1992 <j.murphyjt@gmail.com>
2019-07-29 14:10:35 -0600

    complete DDL - first (complete DDL) push

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -17,3 +17,31 @@ CREATE TABLE `profile` (
    PRIMARY KEY(profileId)
 
 );
+
+CREATE TABLE location (
+	locationId BINARY(16) NOT NULL,
+	locationProfileId BINARY(16) NOT NULL,
+	locationAddress VARCHAR(64) NULL,
+	locationDate DATETIME(6) NOT NULL,
+	locationLatitude VARCHAR(12) NULL,
+	locationLongitude VARCHAR(12) NULL,
+	locationText VARCHAR(300) NOT NULL,
+	locationTitle VARCHAR(64) NOT NULL,
+	locationImdbUrl VARCHAR(255) NOT NULL,
+
+	FOREIGN KEY(locationProfileId) REFERENCES profile(profileId),
+
+	PRIMARY KEY(locationId)
+
+);
+
+CREATE TABLE userLike (
+	userLikeLocationId BINARY(16) NOT NULL,
+	userLikeProfileId BINARY(16) NOT NULL,
+
+	INDEX(userLikeLocationId),
+	INDEX(userLikeProfileId),
+
+	FOREIGN KEY(userLikeLocationId) REFERENCES location(locationId),
+	FOREIGN KEY(userLikeProfileId) REFERENCES  profile(profileId)
+);
