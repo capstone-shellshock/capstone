7221cf88b6d2b9b17e53b49099b52dbe39a03a8a
jmurph1992 <j.murphyjt@gmail.com>
2019-08-01 14:27:49 -0600

    finished class (pre getFoobyBar)

--- php/classes/ValidateUuid.php
+++ php/classes/ValidateUuid.php
@@ -1,6 +1,6 @@
 <?php
 namespace shellShock\Capstone;
-require_once(dirname(__DIR__, 1) . "/classes/autoload.php");
+require_once(dirname(__DIR__, 1) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 /**
  * Trait to validate a uuid
--- php/lib/test.php
+++ php/lib/test.php
@@ -1,1 +1,10 @@
 <?php
+
+namespace shellShock\Capstone;
+
+require_once ("../classes/profile-class.php");
+
+$test = new Profile("0ed44843-effe-49d6-89de-3588b2d9a30f", "f73718223aeefedecabc880f7772c849", "hello@hi.com",
+							"coolGuy", '$argon2i$v=19$m=1024,t=384,p=2$dE55dm5kRm9DTEYxNFlFUA$nNEMItrDUtwnDhZ41nwVm7ncBLrJzjh5mGIjj8RlzVA');
+
+var_dump($test);
