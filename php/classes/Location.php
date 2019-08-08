eaf88ca83c17dece99fc3cea7959ec86dddd4180
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-08-08 17:11:17 -0600

    added get location by location title

--- php/classes/Location.php
+++ php/classes/Location.php
@@ -591,7 +591,7 @@ class Location implements \JsonSerializable {
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when variables are not the correct data type
 	 */
-	public static function getLocationByLocationAddress(\PDO $pdo, string $locationAddress) : Location {
+	public static function getLocationByLocationAddress(\PDO $pdo, string $locationAddress) : \SplFixedArray {
 		//sanitize the description before searching
 		$locationAddress = trim($locationAddress);
 		$locationAddress = filter_var($locationAddress, FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -607,7 +607,7 @@ class Location implements \JsonSerializable {
 		$statement = $pdo->prepare($query);
 
 		//bind the location to the place holder in the template
-		$locationAddress = "%$locationAddress$%";
+		$locationAddress = "%$locationAddress%";
 		$parameters = ["locationAddress" => $locationAddress];
 		$statement->execute($parameters);
 
@@ -624,6 +624,42 @@ class Location implements \JsonSerializable {
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
-		return($location);
+		return($locations);
 	}
+	public static function getLocationByLocationTitle (\PDO $pdo, string $locationTitle) : \SplFixedArray {
+		//sanitize the description before searching
+		$locationTitle = trim($locationTitle);
+		$locationTitle = filter_var($locationTitle, FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($locationTitle) === true) {
+			throw(new \PDOException("location title is invalid"));
+		}
+
+		//escape any mySQL wild cards
+		$locationAddress = str_replace("_","\\_", str_replace("%", "\\%", $locationTitle));
+
+		//create query template
+		$query = "SELECT locationId, locationProfileID, locationAddress, locationDate, locationLatitude, locationLongitude, locationImageCloudinaryId, locationImageCloudinaryUrl, locationText, locationTitle, locationImdbUrl FROM location WHERE locationTitle LIKE :locationTitle ";
+		$statement = $pdo->prepare($query);
+
+		//bind the location to the place holder in the template
+		$locationTitle = "%$locationTitle%";
+		$parameters = ["locationTitle" => $locationTitle];
+		$statement->execute($parameters);
+
+		//build an array of locations
+		$locations = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row =$statement->fetch()) !== false) {
+			try {
+				$location = new Location ($row["locationProfileId"], $row["locationAddress"], $row["locationDate"], $row["locationLatitude"], $row["locationLongitude"], $row["locationImageCloudinaryId"], $row["locationImageCloudinaryUrl"], $row["locationText"], $row["locationTitle"], $row["locationImdbUrl"]);
+				$locations[$locations->key()] = $location;
+				$locations->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($locations);
+	}
+
 }
