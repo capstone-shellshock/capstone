5dd704fb65afbd5bc17e80153e881fc017c6fd4f
Lariah Christensen <lariah_14@hotmail.com>
2019-08-19 09:48:26 -0600

    added files

--- php/lib/like.php
+++ php/lib/like.php
@@ -2,7 +2,7 @@
 
 namespace shellShock\Capstone;
 
-require_once("../Classes/like-class.php");
+require_once("../Classes/Like.php");
 
 $test = new Like ("dc3853da-24ee-427f-a6bd-94d3b0a3d2bc", "2864a877-1805-4b65-ac77-ee4626c75dff");
 
