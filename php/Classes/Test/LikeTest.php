b2511f09fe6596f2df0370849de3f84ff5e5dd1a
Lariah Christensen <lariah_14@hotmail.com>
2019-08-22 13:17:19 -0600

    added file

--- php/Classes/Test/LikeTest.php
+++ php/Classes/Test/LikeTest.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace ShellShock\Capstone\Test;
 
 use ShellShock\Capstone\{Like, Location, Profile};
 
-require_once (dirname(__DIR__)."/autoload.php");
+require_once(dirname(__DIR__) . "/autoload.php");
 
 //grab the UUID generator
-require_once (dirname(__DIR__,2)."/lib/uuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
  * Full PHPUnit test for the Like class
@@ -17,7 +18,6 @@ require_once (dirname(__DIR__,2)."/lib/uuid.php");
  * @see \ShellShock\Capstone\Like
  * @author Lariah Christensen <lchristensen7@cnm.edu>
  **/
-
 class LikeTest extends AbqOnTheReelTest {
 	/**
 	 * Location that was liked; this is for foreign key
@@ -40,7 +40,7 @@ class LikeTest extends AbqOnTheReelTest {
 	/**
 	 * create dependent objects before running each test
 	 **/
-	public final function setUp() : void {
+	public final function setUp(): void {
 		//run the default setUp() method first
 		parent::setUp();
 
@@ -49,11 +49,11 @@ class LikeTest extends AbqOnTheReelTest {
 		$this->VALID_PROFILE_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
 
 //		//create and insert the mocked profile
-		$this->profile = new Profile(generateUuidV4(), null, "test@phpunit.de",$this->VALID_PROFILE_HASH, "phpunit");
+		$this->profile = new Profile(generateUuidV4(), null, "test@phpunit.de", $this->VALID_PROFILE_HASH, "phpunit");
 		$this->profile->insert($this->getPDO());
 
 		//create the and insert the mocked location
-		$this->location = new Location(generateUuidV4(), $this->profile->getProfileId(), "PHPUnit like test passing", null,"-53.4958","89.4938","getcloudiinaryid","anothercloudinaryid", "weneedtacos","ABQONTHEREEL", "https://imbd.movie.url");
+		$this->location = new Location(generateUuidV4(), $this->profile->getProfileId(), "PHPUnit like test passing", null, "-53.4958", "89.4938", "getcloudiinaryid", "anothercloudinaryid", "weneedtacos", "ABQONTHEREEL", "https://imbd.movie.url");
 		$this->location->insert($this->getPDO());
 
 
@@ -62,12 +62,12 @@ class LikeTest extends AbqOnTheReelTest {
 	/**
 	 * test inserting a valid Like and verify that the actual mySQL data matches
 	 **/
-	public function testInsertValidLike() : void {
+	public function testInsertValidLike(): void {
 		//count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("like");
 
 		//create a new Like and insert into mySQL
-		$like = new Like($this->location->getLocationId(),$this->profile->getProfileId());
+		$like = new Like($this->location->getLocationId(), $this->profile->getProfileId());
 		$like->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce the fields match our expectations
@@ -75,48 +75,39 @@ class LikeTest extends AbqOnTheReelTest {
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("like"));
 		$this->assertEquals($pdoLike->getLikeLocationId(), $this->location->getLocationId());
 		$this->assertEquals($pdoLike->getLikeProfileId(), $this->profile->getProfileId());
-			}
+	}
 
-			/**
-			 * test creating a Like and then deleting it
-			 **/
+	/**
+	 * test creating a Like and then deleting it
+	 **/
 
-			public function testDeleteValidLike() : void {
-				//count the number of rows and save it for later
-				$numRows = $this->getConnection()->getRowCount("like");
+	public function testDeleteValidLike(): void {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("like");
 
-				//create a new Like and insert to into mySQL
-				$like = new Like($this->location->getLocationId(),$this->profile->getProfileId());
-				$like->insert($this->getPDO());
+		//create a new Like and insert to into mySQL
+		$like = new Like($this->location->getLocationId(), $this->profile->getProfileId());
+		$like->insert($this->getPDO());
 
-				//delete the Like from mySQL
-				$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("like"));
-				$like->delete($this->getPDO());
+		//delete the Like from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("like"));
+		$like->delete($this->getPDO());
 
-				//grab the data from mySQL and enforce the Location does not exist
-				$pdoLike = Like::getLikeByLikeLocationIdAndLikeProfileId($this->getPDO(),$this->location->getLocationId(), $this->profile->getProfileId());
-				$this->assertNull($pdoLike);
-				$this->assertEquals($numRows, $this->getConnection()->getRowCount("like"));
-			}
-
-//			/**
-//			 * test grabbing a Like that does not exist
-//			 **/
-//			public function testGetInvalidLikeByProfileIdandLocationId() {
-//				//grab a profile id and location id that exceeds the maximum allowable profile id and location id
-//				$like = Like::getLikeByLikeLocationIdAndLikeProfileId($this->getPDO(), generateUuid4(), generateUuidV4());
-//				$this->assertNull($like);
-//			}
+		//grab the data from mySQL and enforce the Location does not exist
+		$pdoLike = Like::getLikeByLikeLocationIdAndLikeProfileId($this->getPDO(), $this->location->getLocationId(), $this->profile->getProfileId());
+		$this->assertNull($pdoLike);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("like"));
+	}
 
 	/**
 	 *test grabbing a Like by location id
 	 **/
-	public function testGetValidLikeByLocationId() : void {
+	public function testGetValidLikeByLocationId(): void {
 		//count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("like");
 
 		//create a new Like and insert to into my SQL
-		$like = new Like($this->location->getLocationId(),$this->profile->getProfileId());
+		$like = new Like($this->location->getLocationId(), $this->profile->getProfileId());
 		$like->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce the fields match our expectations
@@ -131,49 +122,27 @@ class LikeTest extends AbqOnTheReelTest {
 		$this->assertEquals($pdoLike->getLikeProfileId(), $this->profile->getProfileId());
 	}
 
-			/**
-			 *test grabbing a Like by profile id
-			 **/
-			public function testGetValidLikeByProfileId() : void {
-				//count the number of rows and save it for later
-				$numRows = $this->getConnection()->getRowCount("like");
+	/**
+	 *test grabbing a Like by profile id
+	 **/
+	public function testGetValidLikeByProfileId(): void {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("like");
 
-				//create a new Like and insert to into my SQL
-				$like = new Like($this->location->getLocationId(),$this->profile->getProfileId());
-				$like->insert($this->getPDO());
+		//create a new Like and insert to into my SQL
+		$like = new Like($this->location->getLocationId(), $this->profile->getProfileId());
+		$like->insert($this->getPDO());
 
-				//grab the data from mySQL and enforce the fields match our expectations
-				$results = Like::getLikeByProfileId($this->getPDO(), $this->profile->getProfileId());
-				$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("like"));
-				$this->assertCount(1, $results);
-				$this->assertContainsOnlyInstancesOf("ShellShock\\Capstone\\Like", $results);
+		//grab the data from mySQL and enforce the fields match our expectations
+		$results = Like::getLikeByProfileId($this->getPDO(), $this->profile->getProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("like"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("ShellShock\\Capstone\\Like", $results);
 
-				//grab the result from the array and validate it
-				$pdoLike = $results[0];
+		//grab the result from the array and validate it
+		$pdoLike = $results[0];
 
-				$this->assertEquals($pdoLike->getLikeLocationId(), $this->location->getLocationId());
-				$this->assertEquals($pdoLike->getLikeProfileId(), $this->profile->getProfileId());
-			}
-//
-//	/**
-//	 * test grabbing a Like by a profile id that does not exist
-//	 **/
-//	public function testGetInvalidLikeByProfileId() : void {
-//		//grab a location id that exceeds the maximum allowable profile id
-//		$like = Like::getLikebyProfileId($this->getPDO(),generateUuidV4());
-//		$this->assertCount(0, $like);
-//	}
-
-
-
-
-
-//	/**
-//	 * test grabbing a Like by location id that does not exist
-//	 **/
-//	public function testGetInvalidLikeByLocationId() : void {
-//		//grab a location id that exceeds the maximum allowable location id
-//		$like = Like::LikeByLikeLocationId($this->getPDO(), generateUuidV4());
-//		$this->assertCount(0, $like);
-//	}
+		$this->assertEquals($pdoLike->getLikeLocationId(), $this->location->getLocationId());
+		$this->assertEquals($pdoLike->getLikeProfileId(), $this->profile->getProfileId());
 	}
+}
