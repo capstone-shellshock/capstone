de7c7577a5445c209d8c8147d99d4c573eefabd2
jmurph1992 <j.murphyjt@gmail.com>
2019-08-26 13:12:44 -0600

    small error fixes:

--- php/public_html/apis/activation/index.php
+++ php/public_html/apis/activation/index.php
@@ -3,6 +3,7 @@
 
 require_once dirname(__DIR__,3) . "/vendor/autoload.php";
 require_once dirname(__DIR__,3) . "/Classes/autoload.php";
+require_once dirname(__DIR__,3) . "/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/Secrets.php");
 
 use ShellShock\Capstone\Profile;
