ab5100676d60a6f4dc9e5640c1d679e78252e7b7
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-08-25 20:52:32 -0600

    added method get post and delte to location API

--- php/public_html/apis/location/index.php
+++ php/public_html/apis/location/index.php
@@ -8,7 +8,7 @@ require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use ShellShock\Capstone\{Like, Profile, Location};
+use ShellShock\Capstone\{Profile, Location, Like};
 
 /**
  * API for the Location class
@@ -46,21 +46,105 @@ try {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 402));
 	}
 
-	//hande GET request - if id is present, that location is returned, otherwise all locations are returned
+	//handel GET request - if id is present, that location is returned, otherwise all locations are returned
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
 
 		//get a specific location or all locations and update reply
-		if (empty(id) === false) {
+		if(empty(id) === false) {
 			$reply->data = Location::getLocationByLocationId($pdo, $id);
-		} else if (empty($locationProfileId) === false) {
-			// if the user is logged in grap all thwe locations by that used based on who is logged in
+		} else if(empty($locationProfileId) === false) {
+			// if the user is logged in grap all the locations by that used based on who is logged in
 			$reply->data = Location::getLocationByLocationProfileId($pdo, $locationProfileId);
-		}  else if (empty($locationAddress)=== false) {
-		$reply->data = Location::getLocationByLocationAddress($pdo, $locationAddress)->toArray();
+		} else if(empty($locationAddress) === false) {
+			$reply->data = Location::getLocationByLocationAddress($pdo, $locationAddress)->toArray();
+		} else if(empty($locationImdbUrl === false)) {
+			$reply->data = Location::getLocationByLocationImdbUrl($pdo, $locationImdbUrl);
 		} else if(empty($locationText) === false) {
 			$reply->data = Location::getLocationByLocationTitle($pdo, $locationTitle)->toArray();
+		} else if(empty($locationTitle) === false) {
+			$reply->data = Location::getLocationByLocationTitle($pdo, $locationTitle)->toArray();
+		} else {
+			$reply->data = Location::getAllLocations($pdo)->toArray();
+		}
+	} else if($method === "PUT" || $method === "POST") {
+		// enforce the user has a XSRF
+		verifyxsrf();
+
+		//enforce the user is signed in
+		if(empty($_session["profile"]) === true) {
+			throw(new \InvalidArgumentException("you must be logged in to post locations", 401));
+		}
+
+		$requestText = file_get_contents("php://input");
+		//retrieves the JSON that the front end sent, and stores it in $requestContent, here we are using file_get_content ("php//input") o get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+		$requestObject = json_decode($requestText);
+		//this line then decodes the JSON packages and stores that result in $requestObject
+		//make sure location Text is available (required field))
+		if(empty($requestText->locationText) === true) {
+			throw(new \InvalidArgumentException("no text for location", 405));
+		}
+
+		// make sure the location date is accurate(optional field)
+		if(empty($requestObject->locationDate)=== true ) {
+			$requestObject->LocationDate === null;
+		}
+		
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			//retrieve the location to update
+			$location = Location::getLocationByLocationProfileId($pdo, $id);
+			if ($location = null) {
+				throw(new RuntimeException("location does not exist", 404));
+			}
+
+			//enforce the end user has an JWT token
+
+			//enforce the user is signed in and only trying to edit their own Location
+			if (empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $location->getLocationProfileId()->toString()) {
+				throw(new \InvalidArgumentException("you mussed be logged in to post Locations". 403));
+			}
+
+			vaildateJwtHeader();
+
+			//update all attributes
+			$location->setLocationText($requestObject->locationText);
+			$location->update($pdo);
+
+			//update reply
+			$reply->message = "Location update ok";
+
+		} else if ($method === "POST") {
+
+			//enforce the user is signe in
+			if(empty($_SESSION["profile"])=== true) {
+				throw(new \InvalidArgumentException("you must be logged in to post locations", 403));
+			}
+
+			//enforce the end user has JWT token
+			validateJwtHeader();
+
+			//create new location and insert it into the database
+			$location = new Location(generateUuidV4(), $_SESSION["profile"]->getProfileId(), );
+		$location->insert($pdo);
+
+		//update reply
+			$reply->message = "location created ok";
+		}
+
+	}else if($method === "DELET") {
+
+		//enforce that the end user has an XSRF token
+		verifyXsrf();
+
+		//retrieve the Location to be deleted
+		$location = Location::getLocationByLocationId($pdo, $id);
+		if($location === null) {
+			throw(new RuntimeException("Location does not exist", 404));
+		}
+		
 
 	}
 }
