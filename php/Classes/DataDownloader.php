90601023b812909fd1537391094a03577125b37c
jmurph1992 <j.murphyjt@gmail.com>
2019-08-28 10:35:11 -0600

    got data from api w/data downloader - yay

--- php/Classes/DataDownloader.php
+++ php/Classes/DataDownloader.php
@@ -2,10 +2,10 @@
 
 namespace ShellShock\Capstone;
 
-require_once ("autoload.php");
-require_once (dirname(__DIR__) . "/vendor/autoload.php");
-require_once ("/etc/apache2/capstone-mysql/Secrets.php");
-require_once (dirname(__DIR__,1) . "/lib/uuid.php");
+require_once("autoload.php");
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once(dirname(__DIR__, 1) . "/lib/uuid.php");
 
 class DataDownloader {
 
@@ -14,33 +14,36 @@ class DataDownloader {
 		$newLocations = null;
 		$urlBase = "http://coagisweb.cabq.gov/arcgis/rest/services/public/FilmLocations/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&f=pjson";
 		$secrets = new \Secrets("/etc/apache2/capstone-mysql/abqonthereel.ini");
-		$pdo = $secrets -> getPdoObject();
+		$pdo = $secrets->getPdoObject();
 
-		public static function readDataJson($url, $secret) {
+		$newLocations = self::readDataJson($urlBase);
+		var_dump($newLocations);
+	}
 
-			$context = stream_context_create(["http" => ["ignore_errors" => true, "method" => "GET", "header" => "Authorization: Bearer $secret -> apiKey"]]);
+	public static function readDataJson($url) {
 
-			try {
+		$context = stream_context_create(["http" => ["ignore_errors" => true, "method" => "GET"]]);
 
-				//file-get-contents returns file in string context
-				if(($jsonData = file_get_contents($url, null, $context)) === false) {
-					throw(new \RuntimeException("URL does not produce results"));
-				}
+		try {
 
-				//decode the json file
-				$jsonConverted = json_decode($jsonData);
-
-				//format
-				if(empty($jsonConverted -> locations) === false) {
-					$jsonFeatures = $jsonConverted -> locations;
-				}
-				var_dump($jsonFeatures);
-				$newLocations = \SplFixedArray::fromArray($jsonFeatures);
-			} catch(\Exception $exception) {
-				throw(new \PDOException($exception -> getMessage(), 0, $exception));
+			//file-get-contents returns file in string context
+			if(($jsonData = file_get_contents($url, null, $context)) === false) {
+				throw(new \RuntimeException("URL does not produce results"));
 			}
-			return ($newLocations);
+
+			//decode the json file
+			$jsonConverted = json_decode($jsonData);
+
+
+			//format
+			if(empty($jsonConverted->features) === false) {
+				$jsonFeatures = $jsonConverted->features;
+			}
+			$newLocations = \SplFixedArray::fromArray($jsonFeatures);
+		} catch(\Exception $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
+		return ($newLocations);
 	}
 }
 
