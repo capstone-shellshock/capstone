030d62f315ce46705f6e44c387218ee05a1f7313
jmurph1992 <j.murphyjt@gmail.com>
2019-08-31 12:10:57 -0600

    ?

--- php/lib/profileInstantiate.php
+++ php/lib/profileInstantiate.php
@@ -2,11 +2,7 @@
 
 namespace ShellShock\Capstone;
 
-<<<<<<< HEAD
-require_once("../Classes/profile-class.php");
-=======
 require_once("../Classes/Profile.php");
->>>>>>> master
 
 $test = new Profile("0ed44843-effe-49d6-89de-3588b2d9a30f", "f73718223aeefedecabc880f7772c849", "hello@hi.com",
 							"coolGuy", '$argon2i$v=19$m=1024,t=384,p=2$dE55dm5kRm9DTEYxNFlFUA$nNEMItrDUtwnDhZ41nwVm7ncBLrJzjh5mGIjj8RlzVA');
