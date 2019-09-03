e10e1b60313db3d0237ee5fd6deeebf10fae6021
jmurph1992 <j.murphyjt@gmail.com>
2019-09-03 09:57:43 -0600

    experimenting with React

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -4,6 +4,8 @@ export const Home = () => {
 	return (
 		<>
 		<h1>Home</h1>
+	<div>Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+			<i className="fas fa-film"/>
 		</>
 )
-}
+};
