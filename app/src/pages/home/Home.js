b28b155ad45c8470f048ff40258385c250f87bae
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-09-04 14:37:32 -0600

    created static ui for home page

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,10 +1,25 @@
 import React from "react"
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/es/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col"
 
 export const Home = () => {
 	return (
-		<>
-		<h1>Home</h1>
 
-		</>
+		<Container>
+			<Row>
+				<Col md={5}>
+		<h2>Reel Time</h2>
+				</Col>
+				<Col md={5}>
+				<ButtonToolbar className="float-right">
+					<button className="btn btn-light">Make a Scene</button>
+				</ButtonToolbar>
+				</Col>
+			</Row>
+		</Container>
+
 )
 };
