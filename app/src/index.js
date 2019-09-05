8b237f304d66b1e71857c76bda7ec5d6a8712a63
Lariah Christensen <lariah_14@hotmail.com>
2019-09-05 13:48:22 -0600

    added files

--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
-import {FourOhFour} from "./pages/signup/FourOhFour";
-import {Home} from "./pages/home/Home";
-import {NavBar} from "./shared/components/Header";
-import {Location} from "./pages/locations/Locations";
+import {FourOhFour} from "./pages/FourOhFour";
+import {Home} from "./pages/Home";
+import {Signup} from "./pages/Signup";
+import {SignUpForm} from "./pages/SignUpForm";
+
 
 const Routing = () => (
 	<>
