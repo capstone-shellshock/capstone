636f0969e0947d11dba606eac79002ca738fa68b
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-09-05 13:20:56 -0600

    added modal to Location.js

--- app/src/index.js
+++ app/src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
-import {FourOhFour} from "./pages/FourOhFour";
-import {Signup} from "./pages/Signup";
+import {FourOhFour} from "./pages/signup/FourOhFour";
 import {Home} from "./pages/home/Home";
 import {NavBar} from "./shared/components/Header";
+import {Location} from "./pages/locations/Locations";
 
 const Routing = () => (
 	<>
 		<BrowserRouter>
 			<Switch>
-				<Route component ={Home}/>
+				<Route component={Home}/>
 				<Route component={FourOhFour}/>
-
 			</Switch>
 		</BrowserRouter>
 	</>
--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -4,22 +4,39 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/es/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card";
 
 export const Home = () => {
 	return (
-
-		<Container>
-			<Row>
-				<Col md={5}>
-		<h2>Reel Time</h2>
-				</Col>
-				<Col md={5}>
-				<ButtonToolbar className="float-right">
-					<button className="btn btn-light">Make a Scene</button>
-				</ButtonToolbar>
-				</Col>
-			</Row>
-		</Container>
+     <>
+			<Container className="my-top-5 py-lg-5 conte" >
+				<Row>
+					<Col md={3}>
+			<h2>Reel Time</h2>
+					</Col>
+					<Col md={5}>
+					<ButtonToolbar className="float-right">
+						<button className="btn btn-danger">Make a Scene</button>
+					</ButtonToolbar>
+					</Col>
+				</Row>
+			</Container>
+		  <Container>
+		  <Card className="card text-center">
+			  <div className="card-body">
+				  <Card className="text-left">
+					  <Card.Header>Username</Card.Header>
+					  <Card.Body>
+						  <Card.Title>Special title treatment</Card.Title>
+						  <Card.Text>
+							  With supporting text below as a natural lead-in to additional content.
+						  </Card.Text>
+					  </Card.Body>
+				  </Card>
+			  </div>
+		  </Card>
+	  </Container>
+		  </>
 
 )
 };
