e4235e92374df1f88dffb4fdfa3a96f623c5a9b5
jmurph1992 <j.murphyjt@gmail.com>
2019-09-09 14:44:53 -0600

    added reducers for like and profile

---
+++ app/src/shared/reducers/profile-reducer.js
@@ -0,0 +1,12 @@
+export default (state = [], action) => {
+	switch(action.type) {
+		case "GET_ALL_PROFILES":
+			return action.payload;
+		case "GET_PROFILE_BY_PROFILE_ID":
+			return [...state, action.payload];
+		case "GET_PROFILE_BY_PROFILE_EMAIL":
+			return action.payload;
+		default:
+			return state;
+	}
+}
